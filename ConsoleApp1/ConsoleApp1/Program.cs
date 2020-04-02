66230579e6cd5189db3db217ac3248208619820b
Максим Барциц <megapochta650@gmail.com>
2020-04-02 15:26:35 +0300

    hgdytds

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b, c,s;
+            b = "52";
+            c = "Maksim";
+            s = c + b;
+            hgdytds
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
